8c59315ee1cb0b9590e08ce22386ad34c9fec0f4
Drownie <chaoscrafty01@gmail.com>
2023-04-14 18:26:35 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.blueBox} />
+      <View style={styles.redBox} />
+      <View style={styles.greenBox} />
+      <View style={styles.yellowBox} />
     </View>
   );
 }
@@ -13,8 +16,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgray',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  blueBox: {
+    height: '33.3%',
+    width: '50%',
+    backgroundColor: 'royalblue'
+  },
+  redBox: {
+    height: '33.3%',
+    width: '50%',
+    backgroundColor: 'darkred'
+  },
+  greenBox: {
+    height: '33.3%',
+    width: '100%',
+    backgroundColor: 'limegreen'
+  },
+  yellowBox: {
+    height: '33.3%',
+    width: '100%',
+    backgroundColor: 'gold'
   },
 });
